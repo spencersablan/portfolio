ff399a3f894d9c912ea541976f86bd1c770132a3
Spencer Sablan <spencertsablan@gmail.com>
2021-06-08 22:29:26 -0700

    Made responsive for mobile/tablet

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,3 +1,5 @@
+$.mobile.loading().hide()
+
 const projectsToShow = ['#project-1', '#project-2', '#project-3', '#project-4']
 
 const projectQueue = {
@@ -7,32 +9,47 @@ const projectQueue = {
 }
 
 $('#right-arrow').on('click', () => {
-    const {projectShowing, nextProject, previousProject} = projectQueue
+    goToNextProject()
+})
+
+$('#left-arrow').on('click', () => {
+    goToPreviousProject()
+})
+
+$('.projects').on('swipeleft',() => {
+    goToNextProject()
+})
+
+$('.projects').on('swiperight', () => {
+    goToPreviousProject()
+})
+
+const goToNextProject = () => {
+    const {projectShowing, nextProject} = projectQueue
 
     $('#left-arrow').show()
 
-    $(projectsToShow[projectShowing]).hide()
-    $(projectsToShow[nextProject]).show()
+    $(projectsToShow[projectShowing]).animate({left: -100, opacity: "hide"}, 300)
+    $(projectsToShow[nextProject]).delay(100).animate({right: 0, opacity: "show"}, 300)
 
     Object.keys(projectQueue).forEach((value) => projectQueue[value]++)
 
     if (projectQueue.nextProject === projectsToShow.length) {
         $('#right-arrow').hide()
     }
-})
+}
 
-$('#left-arrow').on('click', () => {
-    const {projectShowing, nextProject, previousProject} = projectQueue
+const goToPreviousProject = () => {
+    const {projectShowing, previousProject} = projectQueue
 
     $('#right-arrow').show()
 
-    $(projectsToShow[projectShowing]).hide()
-    $(projectsToShow[previousProject]).show()
+    $(projectsToShow[projectShowing]).animate({right: -100, opacity: "hide"}, 300)
+    $(projectsToShow[previousProject]).delay(100).animate({left: 0, opacity: "show"}, 300)
 
     Object.keys(projectQueue).forEach((value) => projectQueue[value]--)
 
     if (projectQueue.previousProject === -1) {
         $('#left-arrow').hide()
     }
-})
-
+}
